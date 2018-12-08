cc02c14f1ef826e84bb01cbb49d026a078cf4557
Mark Chapman <mark.chapman@gmail.com>
2018-12-08 18:33:11 +0000

    Correct typo and modernise dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -6,62 +6,62 @@ var util = require('gulp-util');
 var templateCache = require('gulp-angular-templatecache');
 
 gulp.task('scss', function() {
-  var appFiles = gulp.src(['demo/scss/demo.scss'])
+  return gulp.src(['demo/scss/demo.scss'])
     .pipe(sass().on('error', util.log))
     .pipe(rename('demo.css'))
     .pipe(gulp.dest('demo/css'));
 });
-
-gulp.task('vendor', function() {
-  var vendorFiles = [
-    "bower_components/jquery/jquery.js",
-    "bower_components/angular/angular.js",
-    "bower_components/moment/moment.js",
-    "bower_components/lodash/dist/lodash.js",
-    "bower_components/rrule/lib/rrule.js",
-    "bower_components/rrule/lib/nlp.js"
-  ];
-
-  return gulp.src(vendorFiles)
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('demo/.'));
-});
-
+//
+// gulp.task('vendor', function() {
+//   var vendorFiles = [
+//     "bower_components/jquery/jquery.js",
+//     "bower_components/angular/angular.js",
+//     "bower_components/moment/moment.js",
+//     "bower_components/lodash/dist/lodash.js",
+//     "bower_components/rrule/lib/rrule.js",
+//     "bower_components/rrule/lib/nlp.js"
+//   ];
+//
+//   return gulp.src(vendorFiles)
+//     .pipe(concat('vendor.js'))
+//     .pipe(gulp.dest('demo/.'));
+// });
+//
 gulp.task('app', function() {
   return gulp.src(['lib/rrule_recurring_select.js'])
     .pipe(concat('app.js'))
     .pipe(gulp.dest('demo/.'));
 });
 
-gulp.task('template-cache', [], function() {
+gulp.task('template-cache', function() {
   return gulp.src(['template/**/*.html'])
     .pipe(templateCache({ root: 'template', module: 'rrule.templates', standalone: true }))
-    .pipe(gulp.dest('demo'));
+    .pipe(gulp.dest('demo/.'));
 });
 
-gulp.task('compile', ['vendor', 'app']);
+gulp.task('compile', gulp.parallel('app'));
 
-gulp.task('build', ['scss', 'compile', 'template-cache']);
+gulp.task('build', gulp.parallel(['scss', 'compile', 'template-cache']));
 
-gulp.task('dist:js', ['build'], function() {
+gulp.task('dist:js', gulp.series('build', function() {
   return gulp.src(['demo/app.js'])
     .pipe(concat('rrule_recurring_select.js'))
     .pipe(gulp.dest('dist'));
-});
+}));
 
-gulp.task('dist:js_with_template', ['build'], function() {
+gulp.task('dist:js_with_template', gulp.series('build', function() {
   return gulp.src(['demo/app.js', 'demo/templates.js'])
     .pipe(concat('rrule_recurring_select_templates.js'))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist/.'));
+}));
 
-gulp.task('dist:css', ['scss'], function() {
+gulp.task('dist:css', gulp.series('scss', function() {
   return gulp.src(['demo/css/demo.css'])
     .pipe(concat('rrule_recurring_select.css'))
-    .pipe(gulp.dest('dist'));
-});
+    .pipe(gulp.dest('dist/.'));
+}));
 
-gulp.task('dist', ['dist:js', 'dist:js_with_template', 'dist:css']);
+gulp.task('dist', gulp.parallel(['dist:js', 'dist:js_with_template', 'dist:css']));
 
 gulp.task('watch', function () {
   gulp.watch('lib/**/*.js', ['compile']);
@@ -70,4 +70,4 @@ gulp.task('watch', function () {
   gulp.watch('template/*.html', ['template-cache']);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', gulp.parallel(['build', 'watch']));
